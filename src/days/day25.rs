3f9697f72735dbf58c91765c8cc769871f2482a3
Wyn Price <wynprice999@gmail.com>
2022-12-25 05:44:50 +0000

    Day 25
    Merry christmas

--- src/days/day25.rs
+++ src/days/day25.rs
@@ -1,7 +1,58 @@
 pub fn part_a(input: &str) -> i64 {
-    panic!("Part A not implimented yet");
+    let sum = parse(input).into_iter().sum();
+    println!("{}", dec_to_snafu(sum, 0, ""));
+    0
 }
 
-pub fn part_b(input: &str) -> i64 {
-    panic!("Part B not implimented yet");
+pub fn part_b(_input: &str) -> i64 {
+    panic!("Merry christmas");
+}
+
+fn parse(input: &str) -> Vec<i64> {
+    input.trim().lines().map(|l| snafu_to_dec(l)).collect()
+}
+
+fn snafu_to_dec(snafu: &str) -> i64 {
+    snafu.chars().rev().enumerate().fold(0, |acc, (index, char)| {
+        let amount: i64 = match char {
+            '0' => 0,
+            '1' => 1,
+            '2' => 2,
+            '-' => -1,
+            '=' => -2,
+            _ => panic!("Unknown {char}")
+        };
+        let val = amount * 5_i64.pow(index as u32);
+        return acc + val;
+    })
+}
+
+fn dec_to_snafu(remains: i64, leftover: i64, total: &str) -> String {
+    if remains == 0 && leftover == 0 {
+        return total.to_owned();
+    }
+    let mut pos = remains % 5 + leftover;
+
+    let overflow = pos >= 5;
+    let mut to_carry = if overflow { 1 } else { 0 };
+    if overflow {
+        pos -= 5;
+    }
+
+    let result = match pos {
+        0 => "0",
+        1 => "1",
+        2 => "2",
+        3 => {
+            to_carry += 1;
+            "="
+        },
+        4 => {
+            to_carry += 1;
+            "-"
+        }
+        _ => panic!("Unknown position {pos}")
+    };
+
+    return dec_to_snafu(remains / 5, to_carry, &(total.to_owned() + result))
 }
