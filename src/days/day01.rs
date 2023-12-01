b2b1df3e93fa072d1c8a9d941ca0035f7737ec03
Wyn Price <github@wynprice.com>
2023-12-01 05:14:03 +0000

    Day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,7 +1,35 @@
 pub fn part_a(input: &str) -> i64 {
-    panic!("Part A not implemented yet")
+    input
+        .lines()
+        .map(|str| {
+            let v = str.chars().filter(|c| c.is_numeric()).collect::<Vec<_>>();
+            return format!("{}{}", v.first().unwrap(), v.last().unwrap());
+        })
+        .map(|str| str.parse::<i64>().unwrap())
+        .sum()
 }
 
 pub fn part_b(input: &str) -> i64 {
-    panic!("Part B not implemented yet")
+    input
+        .lines()
+        .map(|str| {
+            let replaced = str
+                .replace("zero", "zero0zero")
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine");
+            let v = replaced
+                .chars()
+                .filter(|c| c.is_numeric())
+                .collect::<Vec<_>>();
+            return format!("{}{}", v.first().unwrap(), v.last().unwrap());
+        })
+        .map(|str| str.parse::<i64>().unwrap())
+        .sum()
 }
