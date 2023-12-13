f9dd29b3b20d0482fe1d74e6d8965a1954509211
Wyn Price <github@wynprice.com>
2023-12-13 06:25:45 +0000

    Day 13, not too bad just annoying to debug

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -4,128 +4,61 @@ type Area = Vec<Vec<bool>>;
 
 pub fn part_a(input: &str) -> i64 {
     let areas = parse(input);
-    areas
-        .into_iter()
-        .enumerate()
-        .map(|(b, area)| {
-            println!("{b}");
-            rank(area, false)
-        })
-        .sum::<i64>()
+    areas.into_iter().map(|area| rank(area, 0)).sum::<i64>()
 }
 
 pub fn part_b(input: &str) -> i64 {
     let areas = parse(input);
-    areas
-        .into_iter()
-        .enumerate()
-        .map(|(b, area)| {
-            println!("{b}");
-            rank(area, true)
+    areas.into_iter().map(|area| rank(area, 1)).sum::<i64>()
+}
+
+fn rank_dimension(
+    area: &Area,
+    row_differ: usize,
+    max: usize,
+    count_differences: impl Fn(&Area, usize, usize) -> usize,
+) -> i64 {
+    // List of left/above side positions of the mirror
+    // Add tests between the first two and last two elements, as that will be missed when fixing
+    let mut test = vec![0, max - 2];
+    for i in 0..max {
+        for i2 in i + 1..max {
+            if count_differences(&area, i, i2) == 0 {
+                test.push((i + i2 - 1) / 2)
+            }
+        }
+    }
+
+    test.into_iter()
+        .unique()
+        .filter(|t| {
+            let total_not_hit = (0..t + 1)
+                .map(|i| {
+                    // Return false when equal, true when not equal
+                    let i1 = t - i;
+                    let i2 = t + i + 1;
+
+                    // If it's out of bounds, we can assume it does reflect
+                    if i2 >= max {
+                        return 0;
+                    }
+
+                    return count_differences(&area, i1, i2);
+                })
+                .sum::<usize>();
+
+            return total_not_hit == row_differ;
         })
+        .map(|t| 1 + t as i64)
         .sum::<i64>()
 }
 
-fn rank(area: Area, fix: bool) -> i64 {
+fn rank(area: Area, row_differ: usize) -> i64 {
     let height = area.len();
     let width = area[0].len();
 
-    let mut sum = 0;
-
-    // Check vertical mirrors
-    // List of left side positions of the mirror
-    // Add tests between the first two and last two elements, as that will be missed when fixing
-    let mut test_vertical = vec![0, width - 2];
-    for x in 0..width {
-        for xc in x + 1..width {
-            if col_eq(&area, x, xc) {
-                test_vertical.push((x + xc - 1) / 2)
-            }
-        }
-    }
-
-    sum += test_vertical
-        .into_iter()
-        .unique()
-        .filter_map(|tv| {
-            let not_hit = (0..tv + 1)
-                .map(|y| {
-                    // Return false when equal, true when not equal
-                    let x1 = tv - y;
-                    let x2 = tv + y + 1;
-
-                    if x2 >= width {
-                        return 0;
-                    }
-
-                    return col_ne_c(&area, x1, x2);
-                })
-                .collect_vec();
-            if fix {
-                if not_hit.into_iter().sum::<usize>() == 1 {
-                    println!("  Flip V {tv}");
-                    return Some(1 + tv as i64);
-                }
-            } else if !not_hit.into_iter().any(|d| d != 0) {
-                println!("  Vertical {tv}");
-                return Some(1 + tv as i64);
-            }
-            return None;
-        })
-        .sum::<i64>();
-
-    // Check horizontal mirrors
-    // List of upwards side positions of the mirror
-    // Add tests between the first two and last two elements, as that will be missed when fixing
-    let mut test_horizontal = vec![0, height - 2];
-    for y in 0..height {
-        for yc in y + 1..height {
-            if row_eq(&area, y, yc) {
-                test_horizontal.push((y + yc - 1) / 2)
-            }
-        }
-    }
-
-    sum += test_horizontal
-        .into_iter()
-        .unique()
-        .filter_map(|th| {
-            let not_hit = (0..th + 1)
-                .map(|x| {
-                    // Return false when equal, true when not equal
-                    let y1 = th - x;
-                    let y2 = th + x + 1;
-
-                    if y2 >= height {
-                        return 0;
-                    }
-
-                    return row_ne_c(&area, y1, y2);
-                })
-                .collect_vec();
-            if fix {
-                if not_hit.into_iter().sum::<usize>() == 1 {
-                    println!("  Flip H {th}");
-                    return Some(1 + th as i64);
-                }
-            } else if !not_hit.into_iter().any(|d| d != 0) {
-                println!("  Horizontal {th}");
-                return Some(1 + th as i64);
-            }
-            return None;
-        })
-        .sum::<i64>()
-        * 100;
-
-    sum
-}
-
-fn col_eq(area: &Area, x1: usize, x2: usize) -> bool {
-    !(0..area.len()).any(|y| area[y][x1] != area[y][x2])
-}
-
-fn row_eq(area: &Area, y1: usize, y2: usize) -> bool {
-    !(0..area[0].len()).any(|x| area[y1][x] != area[y2][x])
+    rank_dimension(&area, row_differ, width, col_ne_c)
+        + 100 * rank_dimension(&area, row_differ, height, row_ne_c)
 }
 
 fn col_ne_c(area: &Area, x1: usize, x2: usize) -> usize {
