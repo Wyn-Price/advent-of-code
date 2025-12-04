445b65bd3d7d4ca05ed1449f1aa7eb6e08dc15b3
Wyn Price <github@wynprice.com>
2025-12-04 09:15:15 +0000

    Day 4

--- src/years/y2025/day04.rs
+++ src/years/y2025/day04.rs
@@ -1,7 +1,79 @@
+use std::collections::HashSet;
+
 pub fn part_a(input: &str) -> i64 {
-    panic!("Part A not implemented yet");
+    let set = parse(input);
+
+    let mut sum = 0;
+
+    let copy = set.iter().copied().collect::<Vec<_>>();
+    for (x, y) in copy {
+        let mut around = 0;
+        for xi in -1..=1 {
+            for yi in -1..=1 {
+                if xi == 0 && yi == 0 {
+                    continue;
+                }
+
+                if set.contains(&(x + xi, y + yi)) {
+                    around += 1;
+                }
+            }
+        }
+
+        if around < 4 {
+            sum += 1;
+        }
+    }
+
+    return sum;
 }
 
 pub fn part_b(input: &str) -> i64 {
-    panic!("Part B not implemented yet");
+    let mut set = parse(input);
+
+    let mut sum = 0;
+
+    loop {
+        let mut changed = false;
+        let copy = set.iter().copied().collect::<Vec<_>>();
+        for (x, y) in copy {
+            if !set.contains(&(x, y)) {
+                continue;
+            }
+            let mut around = 0;
+            for xi in -1..=1 {
+                for yi in -1..=1 {
+                    if xi == 0 && yi == 0 {
+                        continue;
+                    }
+
+                    if set.contains(&(x + xi, y + yi)) {
+                        around += 1;
+                    }
+                }
+            }
+
+            if around < 4 {
+                sum += 1;
+                set.remove(&(x, y));
+                changed = true;
+            }
+        }
+
+        if !changed {
+            break;
+        }
+    }
+
+    return sum;
+}
+
+fn parse(input: &str) -> HashSet<(i64, i64)> {
+    return input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, s)| s.char_indices().map(move |(x, c)| (x, y, c)))
+        .filter(|(_, _, c)| *c == '@')
+        .map(|(x, y, _)| (x as i64, y as i64))
+        .collect();
 }
