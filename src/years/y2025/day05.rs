86f4c3949bfcadc66fc55415db0e1a67b2a30203
Wyn Price <github@wynprice.com>
2025-12-05 09:41:33 +0000

    Day 5

--- src/years/y2025/day05.rs
+++ src/years/y2025/day05.rs
@@ -1,7 +1,50 @@
+use std::ops::RangeInclusive;
+
 pub fn part_a(input: &str) -> i64 {
-    panic!("Part A not implemented yet");
+    let (ranges, nums) = parse(input);
+
+    nums.into_iter()
+        .filter(|x| ranges.iter().any(|range| range.contains(x)))
+        .count() as i64
 }
 
 pub fn part_b(input: &str) -> i64 {
-    panic!("Part B not implemented yet");
+    let (mut ranges, _) = parse(input);
+
+    ranges.sort_by_key(|r| *r.start());
+
+    let mut merged: Vec<RangeInclusive<i64>> = vec![];
+
+    for r in ranges {
+        if let Some(last) = merged.last_mut() {
+            if r.start() <= last.end() {
+                let &s = last.start();
+                let &e = last.end().max(r.end());
+                *last = s..=e;
+                continue;
+            }
+        }
+        merged.push(r);
+    }
+
+    merged.into_iter().map(|r| r.end() - r.start() + 1).sum()
+}
+
+fn parse(input: &str) -> (Vec<RangeInclusive<i64>>, Vec<i64>) {
+    let (ranges, nums) = input.split_once("\n\n").unwrap();
+
+    return (
+        ranges
+            .lines()
+            .into_iter()
+            .map(|line| {
+                let (l, r) = line.split_once("-").unwrap();
+                return l.parse::<i64>().unwrap()..=r.parse::<i64>().unwrap();
+            })
+            .collect(),
+        nums.lines()
+            .into_iter()
+            .map(|l| l.parse().unwrap())
+            .collect(),
+    );
 }
