14a9101b067f6e48c429933c6c1fcfbfbba74549
Wyn Price <github@wynprice.com>
2025-12-07 13:34:49 +0000

    Day 6 + 7

--- src/years/y2025/day06.rs
+++ src/years/y2025/day06.rs
@@ -1,7 +1,84 @@
+use itertools::Itertools;
+
 pub fn part_a(input: &str) -> i64 {
-    panic!("Part A not implemented yet");
+    let (line, ops) = parse(input);
+
+    let mut total = 0;
+    for column in 0..ops.len() {
+        let mut sum = line[0][column];
+        for row in 1..line.len() {
+            match ops[column] {
+                '+' => sum += line[row][column],
+                '*' => sum *= line[row][column],
+                _ => panic!("{}", ops[column]),
+            }
+        }
+
+        total += sum;
+    }
+
+    return total;
 }
 
 pub fn part_b(input: &str) -> i64 {
-    panic!("Part B not implemented yet");
+    let mut total = 0;
+
+    let lines = input.lines().collect_vec();
+    let rows = lines[0].len();
+
+    let mut current_nums: Vec<i64> = vec![];
+    let mut current_op = None;
+
+    for x in 0..=rows {
+        let mut num = String::new();
+        for y in 0..lines.len() - 1 {
+            let c = lines[y].chars().nth(x).unwrap_or(' ');
+            if c != ' ' {
+                num.push(c);
+            }
+        }
+
+        let c = lines[lines.len() - 1].chars().nth(x).unwrap_or(' ');
+        if c != ' ' {
+            current_op = Some(c);
+        }
+
+        if num == "" {
+            let mut sum = current_nums.remove(0);
+            for ele in current_nums {
+                match current_op.unwrap() {
+                    '+' => sum += ele,
+                    '*' => sum *= ele,
+                    _ => panic!(),
+                }
+            }
+
+            total += sum;
+            current_nums = vec![];
+            current_op = None;
+        } else {
+            current_nums.push(num.parse().unwrap());
+        }
+    }
+
+    return total;
+}
+
+fn parse(input: &str) -> (Vec<Vec<i64>>, Vec<char>) {
+    let mut lines = input.lines().collect_vec();
+    let ops = lines.pop().unwrap();
+
+    return (
+        lines
+            .into_iter()
+            .map(|l| {
+                l.split_whitespace()
+                    .map(|n| n.parse().unwrap())
+                    .collect_vec()
+            })
+            .collect_vec(),
+        ops.split_whitespace()
+            .map(|o| o.chars().nth(0).unwrap())
+            .collect_vec(),
+    );
 }
--- src/years/y2025/day07.rs
+++ src/years/y2025/day07.rs
@@ -1,7 +1,72 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
 pub fn part_a(input: &str) -> i64 {
-    panic!("Part A not implemented yet");
+    let (start, height, pos) = parse(input);
+
+    let mut beams = HashSet::new();
+    beams.insert(start.0);
+
+    let mut times_split = 0;
+
+    for y in 1..height {
+        let mut new_beams = HashSet::new();
+        for x in beams {
+            if pos.contains(&(x, y)) {
+                new_beams.insert(x - 1);
+                new_beams.insert(x + 1);
+                times_split += 1
+            } else {
+                new_beams.insert(x);
+            }
+        }
+        beams = new_beams;
+    }
+
+    return times_split;
 }
 
 pub fn part_b(input: &str) -> i64 {
-    panic!("Part B not implemented yet");
+    let (start, height, pos) = parse(input);
+
+    let mut beams = vec![(start.0, 1_i64)];
+
+    for y in 1..height {
+        println!("{y} / {height}");
+        let mut new_beams = vec![];
+        for (x, count) in beams {
+            if pos.contains(&(x, y)) {
+                new_beams.push((x - 1, count));
+                new_beams.push((x + 1, count));
+            } else {
+                new_beams.push((x, count));
+            }
+        }
+        beams = new_beams
+            .into_iter()
+            .into_group_map_by(|(x, _)| *x)
+            .into_iter()
+            .map(|(x, values)| (x, values.into_iter().map(|(_, count)| count).sum()))
+            .collect_vec();
+    }
+
+    return beams.into_iter().map(|b| b.1).sum();
+}
+
+fn parse(input: &str) -> ((i64, i64), i64, HashSet<(i64, i64)>) {
+    let mut start = None;
+    let mut positions = HashSet::new();
+
+    input.lines().enumerate().for_each(|(y, l)| {
+        l.char_indices().for_each(|(x, c)| {
+            if c == 'S' {
+                start = Some((x as i64, y as i64));
+            } else if c == '^' {
+                positions.insert((x as i64, y as i64));
+            }
+        })
+    });
+
+    return (start.unwrap(), input.lines().count() as i64, positions);
 }
